a358142423037923795dd7e606f9fdff6536a9e5
nikiforov.av <nikiforov.av@dclouds.ru>
2022-07-22 15:39:49 +0700

    added everyday chart

---
+++ src/components/General/Charts/EverydayViewsChart/EverydayViewsChart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EverydayViewsChart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EverydayViewsChart;
--- src/pages/general/index.tsx
+++ src/pages/general/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ViewsChart from "../../components/General/Charts/ViewsChart/ViewsChart";
+import {Box} from "@mui/material";
 
 const General = () => {
     return (
         <div>
             <div className="admin-title">Привет, Андрей!</div>
-            <div className="first-chart">
-                Всего просмотров
-                <ViewsChart />
-            </div>
+            <Box sx={{ display: 'flex' }}>
+                <div className="first-chart">
+                    Всего просмотров
+                    <ViewsChart />
+                </div>
+            </Box>
+
         </div>
     );
 };
